61f808822edd6942e4aee106083307f424bcb5b8
Miroslav Stoev <soboko@abv.bg>
2024-10-08 14:14:58 +0300

    React components

---
+++ react-components/src/components/Heading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const customStyle = {
+  textAlign: "center",
+  color: "blue",
+};
+
+function Heading() {
+  return <h1 style={customStyle}>My Favourite Foods</h1>;
+}
+
+export default Heading;
---
+++ react-components/src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Tomato</li>
+      <li>Bacon</li>
+      <li>Orange</li>
+    </ul>
+  );
+}
+
+export default List;
