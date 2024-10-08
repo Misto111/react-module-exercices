e4baf0870233a9e064f2ea9d6a7d72048941539f
Miroslav Stoev <soboko@abv.bg>
2024-10-08 11:09:11 +0300

    Adding new exercise

---
+++ inline-styling-in-jsx/src/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black",
+};
+
+customStyle.color = "blue";
+
+ReactDOM.render(
+  <h1 style={customStyle}>This is cool!</h1>,
+  document.getElementById("root")
+);
+
+
