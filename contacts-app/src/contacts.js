48f14c04388659378bf6b498a133500e00c17a99
Miroslav Stoev <soboko@abv.bg>
2024-10-09 12:27:59 +0300

    Adding the contacts-app

---
+++ contacts-app/src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name: "Beyonce",
+    imgURL:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    phone: "+123 456 789",
+    email: "b@beyonce.com",
+  },
+  {
+    name: "Goldi",
+    imgURL:
+      "https://static.wixstatic.com/media/f2c7ab_17e00adeeeec44309750a46e5718a30f~mv2.jpeg/v1/fill/w_980,h_784,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/f2c7ab_17e00adeeeec44309750a46e5718a30f~mv2.jpeg",
+    phone: "+987 654 321",
+    email: "goldi@nowhere.com",
+  },
+  {
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com",
+  },
+];
+
+export default contacts;
