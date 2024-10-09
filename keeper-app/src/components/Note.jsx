7ed65e1f4eaccdcd34b49c46676d0b0600dd218c
Miroslav Stoev <soboko@abv.bg>
2024-10-09 10:17:54 +0300

    keeper-app

---
+++ keeper-app/src/components/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const title = "Title";
+const content = "Content";
+
+function Footer() {
+  return (
+    <div className="note">
+      <h1>{title}</h1>
+      <p>{content}</p>
+    </div>
+  );
+}
+
+export default Footer;
